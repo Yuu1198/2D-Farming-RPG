0a6c7613e5b963e57dda86198ec11ca5ded75bcd
Yuu1198 <nadia.hofer1998@outlook.com>
2024-07-08 13:34:00 +0200

    Set up player character.

---
+++ 2D-RPG/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+
+    public Animator animator;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        Vector3 direction = new Vector3(horizontal, vertical).normalized;
+
+        AnimateMovement(direction, isRunning);
+
+        transform.position += direction * currentSpeed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction, bool isRunning)
+    {
+        if(animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
