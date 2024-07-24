08cca1e0c86b3499bba01344814df17a295822dd
Yuu1198 <nadia.hofer1998@outlook.com>
2024-07-24 11:29:47 +0200

    Created collactable object.

---
+++ 2D-RPG/Assets/Scripts/Collectable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null )
+        {
+            player.numCarrotSeed++;
+            Destroy(this.gameObject);
+        }
+    }
+}
